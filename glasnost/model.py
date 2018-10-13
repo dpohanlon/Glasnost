Fix normalisation for real this time.

glasnost/model.py
@@ -353,11 +353,8 @@ class Model(Distribution):
         return {name : c.integral(minVal, maxVal) for (name, c) in self.fitComponents.items()}
 
     def integral(self, minVal, maxVal):
-        # Operate on already normalised PDFs
-        # Screws up normalisation over range?!?!????!?!?
-        # return 1.0
 
-        return np.sum(list(self.getComponentIntegrals(minVal, maxVal).values()))
+        return np.sum(list(self.getComponentIntegrals(minVal, maxVal).values())) / len(self.getComponentIntegrals(minVal, maxVal).values())
 
     def sample(self, nEvents = None, minVal = None, maxVal = None):
         # Generate according to yields and component models

tests/tests.py
@@ -285,6 +285,8 @@ def testDoubleGaussianFrac():
 
 if __name__ == '__main__':
 
-    # print(testSimpleCB())
-    # print(testDoubleGaussianYields())
-    print(testDoubleGaussianFrac())
+    print(np.sum([testSimpleCB(),
+                  testDoubleGaussianYields(),
+                  testDoubleGaussianFrac(),
+                  testSimpleGaussian(),
+                  ] ))